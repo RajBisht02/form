e225ccf4726603a8317d2176c843ec1f0bd19915
RajBisht02 <77607723+RajBisht02@users.noreply.github.com>
2022-05-12 03:33:49 +0530

    Add files via upload

---
+++ src/components/BottomContainer.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+
+
+function BottomContainer() {
+    
+  return (
+    <div className='bottom'>
+        Footer
+    </div>
+  )
+}
+
+export default BottomContainer;
---
+++ src/components/TopContainer.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+
+
+function TopContainer() {
+    
+  return (
+    <div className='top'>
+        <div className='logo'>
+            Logo
+        </div>
+        <div className='header'>
+            Header
+        </div>
+        <div className='contact'>
+            contact
+        </div>
+        <div className='admin'>
+            admin
+        </div>
+    </div>
+  )
+}
+
+export default TopContainer;
